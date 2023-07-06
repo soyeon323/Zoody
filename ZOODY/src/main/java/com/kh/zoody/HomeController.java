17d9cb5f9752bab40461ab3b88abfea5c8d0fb3c
DESKTOP-PK5D7TE\LG <yoonssong123@naver.com>
2023-07-06 17:35:07 +0900

    홈화면 커밋

--- ZOODY/src/main/java/com/kh/zoody/HomeController.java
+++ ZOODY/src/main/java/com/kh/zoody/HomeController.java
@@ -1,6 +1,7 @@
 package com.kh.zoody;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 	
 	@ResponseBody
+	@GetMapping
 	public String home() {
 		return "home";
 	}
