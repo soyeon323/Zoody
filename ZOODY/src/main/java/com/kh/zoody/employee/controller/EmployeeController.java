6d061b2008552bd830aa52211e3a6477be5ced06
DESKTOP-PK5D7TE\LG <yoonssong123@naver.com>
2023-07-10 01:47:13 +0900

    직원등록기능 커밋

--- ZOODY/src/main/java/com/kh/zoody/employee/controller/EmployeeController.java
+++ ZOODY/src/main/java/com/kh/zoody/employee/controller/EmployeeController.java
@@ -1,4 +1,4 @@
-package com.kh.zoody.employee.enroll;
+package com.kh.zoody.employee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("employee")
-public class EmployeeEnrollController {
+public class EmployeeController {
 
 	@GetMapping("enroll")
 	public void enroll() {}
