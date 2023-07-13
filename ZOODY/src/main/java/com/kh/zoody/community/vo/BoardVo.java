3bd4b22a50d3f5349f7a8c229fed3ebbda12cbf2
DESKTOP-PKLA6F4\user2 <>
2023-07-13 21:46:42 +0900

    커밋

---
+++ ZOODY/src/main/java/com/kh/zoody/community/vo/BoardVo.java
@@ -0,0 +1,18 @@
+package com.kh.zoody.community.vo;
+
+import lombok.Data;
+
+@Data
+public class BoardVo {
+	
+	private String no;
+	private String user_no;
+	private String no2;
+	private String title;
+	private String content;
+	private String enoll_date;
+	private String modify_date;
+	private String status;
+	private String file;
+	
+}
