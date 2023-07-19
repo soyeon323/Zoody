2fb6df922868ab6e7710bca86779b6dd008be333
CcoldCat <ccoldcat@gmail.com>
2023-07-20 08:45:33 +0900

    글쓰기 화면 작업 + 날씨 수정중

--- ZOODY/src/main/java/com/kh/zoody/api/weather/controller/WeatherApiController.java
+++ ZOODY/src/main/java/com/kh/zoody/api/weather/controller/WeatherApiController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
+import com.kh.zoody.api.weather.setting.WeatherSetting;
 import com.kh.zoody.api.weather.vo.WeatherVo;
 
 import lombok.RequiredArgsConstructor;
@@ -33,23 +34,21 @@ public class WeatherApiController {
 	@GetMapping("/weather")
 	public List weatherInfo() throws Exception {
 		
-		WeatherVo vo = new WeatherVo(null, null, 0, null, null, null, 0, 0);
-		
-		
+		WeatherVo vo =  new WeatherVo();
 		
 		//날씨정보 얻기
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") +"=" + vo.getServiceKey() ); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8") ); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(vo.getPageNo(), "UTF-8") ); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8") ); /*요청자료형식(XML/JSON) Default: XML*/
-        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(vo.getBaseDate(), "UTF-8") ); /*‘21년 6월 28일발표*/
-        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(vo.getBaseTime(), "UTF-8") ); /*05시 발표*/
-        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8") ); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8") ); /*예보지점의 Y 좌표값*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") +"=" + WeatherSetting.SERVICE_KEY ); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + Integer.toString(WeatherSetting.PAGE_NO) ); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(Integer.toString(WeatherSetting.NUM_OF_ROWS), "UTF-8") ); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode(WeatherSetting.DATA_TYPE, "UTF-8") ); /*요청자료형식(XML/JSON) Default: XML*/
+        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(WeatherSetting.BASE_DATE, "UTF-8") ); /*‘21년 6월 28일발표*/
+        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(WeatherSetting.BASE_TIME, "UTF-8") ); /*05시 발표*/
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(Integer.toString(WeatherSetting.NX), "UTF-8") ); /*예보지점의 X 좌표값*/
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(Integer.toString(WeatherSetting.NY), "UTF-8") ); /*예보지점의 Y 좌표값*/
         URL url = new URL(urlBuilder.toString());
         
-        log.info(url.toString());
+//        log.info(url.toString())
         
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -79,7 +78,9 @@ public class WeatherApiController {
         Map itmes = (Map) body.get("items");
         List itemList = (List) itmes.get("item");
         
-        log.info("itemList : "+itemList.toString());
+        
+        System.out.println(itemList.toString());
+//        log.info("itemList : "+itemList.toString());
 		
 		return itemList;
 	}
--- ZOODY/src/main/webapp/WEB-INF/views/common/setup.jsp
+++ ZOODY/src/main/webapp/WEB-INF/views/common/setup.jsp
@@ -12,13 +12,21 @@
 <!-- context root set -->
 <c:set var="root" value="${pageContext.request.contextPath}" />
 
+<!-- include libraries(jQuery, bootstrap) -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 <!-- css 기본 세팅 -->
 <link rel="stylesheet" href="${root}/resources/css/common/setup.css">
 
+<!-- summernote 세팅 css/js -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
 <!-- js 기본설정 -->
 <script>
     // root 설정 || 사용 예제  url : root + "/api/weather"
-    const hostIndex = location.href.indexOf( location.host ) + location.host.length;
-    const root = location.href.substring( hostIndex, location.href.indexOf('/', hostIndex + 1) );
+    var hostIndex = location.href.indexOf( location.host ) + location.host.length;
+    var root = location.href.substring( hostIndex, location.href.indexOf('/', hostIndex + 1) );
 </script>
