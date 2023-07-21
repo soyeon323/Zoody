ddb0ecc56f080e029155abcc73a3ebf4d4f78c8c
DESKTOP-O33TV15\user2 <>
2023-07-21 19:13:35 +0900

    로그인 및 비밀번호 설정시 몇개의 조건과 암호화 작업 완료

--- ZOODY/src/main/java/com/kh/zoody/pwd/dao/PwdSettingDao.java
+++ ZOODY/src/main/java/com/kh/zoody/pwd/dao/PwdSettingDao.java
@@ -10,7 +10,12 @@ public class PwdSettingDao {
 	
 	//비밀번호 설정을 위한 아이디(사번)체크 로직
 	public int idCheck(SqlSessionTemplate sst, UserVo vo) {
-		return sst.selectOne("login.pwdSetting" ,vo);
+		return sst.selectOne("login.idCheck" ,vo);
+	}
+
+	//비밀번호 설정 (암호화)
+	public int pwdSetting(SqlSessionTemplate sst, UserVo vo) {
+		return sst.update("login.pwdSetting" ,vo);
 	}
 
 }
