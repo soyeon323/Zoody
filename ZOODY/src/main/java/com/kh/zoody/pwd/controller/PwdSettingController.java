d92b534f62fbfb8503b46a9bede259a2e84e7276
DESKTOP-S2F5795\kimdoyeon <ghdh21@naver.com>
2023-07-21 12:59:54 +0900

    로그인 기능 확인 완료했고 비밀번호 설정시 id(사번) 인증하는거 구현중...

--- ZOODY/src/main/java/com/kh/zoody/pwd/controller/PwdSettingController.java
+++ ZOODY/src/main/java/com/kh/zoody/pwd/controller/PwdSettingController.java
@@ -2,14 +2,42 @@ package com.kh.zoody.pwd.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kh.zoody.pwd.service.PwdSettingService;
+import com.kh.zoody.user.vo.UserVo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping("pwd/setting")
+@RequestMapping("pwd")
+@RequiredArgsConstructor
+@Slf4j
 public class PwdSettingController {
 
-	@GetMapping
+	private final PwdSettingService ps;
+	
+	//비밀번호 설정 뷰 페이지로 이동
+	@GetMapping("setting")
 	public String pwdSetting() {
 		return "pwd/setting";
+	}
+	
+	
+	//비밀번호 설정시 AJAX 로 아이디 인증 확인 로직
+	@PostMapping("setting")
+	@ResponseBody
+	public int pwdSetting(UserVo vo) {
+		
+		int result = ps.idCheck(vo);
+		
+		if(result !=1) {
+			throw new RuntimeException();
+		}
+		log.info("resutl : {}" , result);
+		return result;
 	}
 }
--- ZOODY/src/main/java/com/kh/zoody/pwd/service/PwdSettingService.java
+++ ZOODY/src/main/java/com/kh/zoody/pwd/service/PwdSettingService.java
@@ -1,10 +1,10 @@
-package com.kh.zoody.login.service;
+package com.kh.zoody.pwd.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.kh.zoody.login.dao.LoginDao;
+import com.kh.zoody.pwd.dao.PwdSettingDao;
 import com.kh.zoody.user.vo.UserVo;
 
 import lombok.RequiredArgsConstructor;
@@ -13,13 +13,16 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class LoginService {
+@Slf4j
+public class PwdSettingService {
 	
-	private final LoginDao dao;
+	private final PwdSettingDao psd;
 	private final SqlSessionTemplate sst;
-	
-	//로그인 
-	public UserVo login(UserVo vo) {
-		return dao.login(sst,vo);
+
+	// 비밀번호 설정을 위한 아이디(사번) 체크 로직
+	public int idCheck(UserVo vo) {
+		return psd.idCheck(sst, vo);
 	}
+	
+	
 }
