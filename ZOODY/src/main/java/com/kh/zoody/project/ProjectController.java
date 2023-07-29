af5e7433e9d30b1919e77f45123aba5547e21248
DESKTOP-PK5D7TE\LG <yoonssong123@naver.com>
2023-07-30 00:27:28 +0900

    프로젝트 생성 기능

--- ZOODY/src/main/java/com/kh/zoody/project/ProjectController.java
+++ ZOODY/src/main/java/com/kh/zoody/project/ProjectController.java
@@ -1,18 +1,34 @@
 package com.kh.zoody.project;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.zoody.project.service.ProjectService;
+import com.kh.zoody.project.vo.ProjectVo;
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("project")
 @RequiredArgsConstructor
+@Slf4j
 public class ProjectController {
+	private final ProjectService ps;
 
 	//참여중인 프로젝트 화면
 	@RequestMapping("progress")
-	public void projectInProgress() {}
+	public void projectInProgress(Model model) {
+		List<ProjectVo> voList = ps.selectUser();
+		
+		if(voList == null) {
+			throw new RuntimeException();
+		}
+		model.addAttribute("voList", voList);
+	}
 	
 	//프로젝트 상세화면
 	@RequestMapping("detail")
---
+++ ZOODY/src/main/java/com/kh/zoody/project/vo/ProjectVo.java
@@ -0,0 +1,16 @@
+package com.kh.zoody.project.vo;
+
+import lombok.Data;
+
+@Data
+public class ProjectVo {
+	private String no;
+	private String title;
+	private String content;
+	private String startDate;
+	private String endDate;
+	private String teamName;
+	
+	private String userNo;
+	private String projectNo;
+}
